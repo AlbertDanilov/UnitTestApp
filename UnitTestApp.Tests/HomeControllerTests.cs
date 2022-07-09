f18715ca8ee2b87c392eff8ca912ad93715f40b6
Danilov <albert95danilov@gmail.com>
2022-07-09 11:54:11 +0300

    tests

---
+++ UnitTestApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTestApp.Controllers;
+using Xunit;
+
+namespace UnitTestApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexViewDataMessage()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.Equal("Hello world!", result?.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void IndexViewResultNotNull()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void IndexViewNameEqualIndex()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.Equal("Index", result?.ViewName);
+        }
+
+        [Fact]
+        public void IndexTest()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.Equal("Hello world!", result?.ViewData["Message"]);
+            Assert.NotNull(result);
+            Assert.Equal("Index", result?.ViewName);
+        }
+    }
+}
